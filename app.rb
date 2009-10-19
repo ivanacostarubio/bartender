8f595d0c29c30b54cf9426546b79ba37f13c4313
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-10-19 13:09:49 -0400

    initial sinatra app

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'sinatra'
+
+
+helpers do
+  def partial(page, options={})
+    haml page, options.merge!(:layout => false)
+  end
+end
+
+
+get '/' do
+  haml :layout
+end
+
+
+
+
